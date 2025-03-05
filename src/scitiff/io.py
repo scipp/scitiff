d14234d03aa24d2df9affa42d89a187c4ea6f258
YooSunyoung <luysunyoung9@gmail.com>
2025-03-05 14:56:53 +0100

    Handle squeezing.

--- src/scitiff/io.py
+++ src/scitiff/io.py
@@ -16,32 +16,42 @@ from ._schema import (
 )
 
 
-def _ensure_dimension_order(sizes: dict) -> dict:
-    # Order of the dimensions is according to the HyperStacks tiff format.
-    order = ['c', 't', 'z', 'y', 'x']
-    return {key: sizes[key] for key in order if key in sizes}
-
-
 def _scipp_variable_to_model(var: sc.Variable) -> ScippVariable:
+    import warnings
+
+    if var.ndim > 1:
+        warnings.warn(
+            "The variable has more than 1 dimension. "
+            "Variables for metadata should not take too much space. "
+            "Consider using single dimension variable for metadata.",
+            ResourceWarning,
+            stacklevel=2,
+        )
     return ScippVariable(
         dims=tuple(var.dims),
         dtype=str(var.dtype),
         shape=tuple(var.shape),
         unit=str(var.unit),
         values=var.values.tolist(),
+        variances=None if var.variances is None else var.variances.tolist(),
+    )
+
+
+def _scipp_variable_to_metadata_model(var: sc.Variable) -> ImageVariableMetadata:
+    # Image data variable should have more than 1 dimension
+    # So we do not warn the user about the multi-dimensional variable
+    return ImageVariableMetadata(
+        dims=tuple(var.dims),
+        dtype=str(var.dtype),
+        shape=tuple(var.shape),
+        unit=str(var.unit),
+        variances=None if var.variances is None else var.variances.tolist(),
     )
 
 
 def _extract_metadata_from_dataarray(da: sc.DataArray) -> ImageDataArrayMetadata:
-    default_sizes = {'x': 1, 'y': 1, 'z': 1, 't': 1, 'c': 1}
-    final_sizes = _ensure_dimension_order({**default_sizes, **da.sizes})
     return ImageDataArrayMetadata(
-        data=ImageVariableMetadata(
-            dims=tuple(final_sizes.keys()),
-            shape=tuple(final_sizes.values()),
-            unit=str(da.unit),
-            dtype=str(da.dtype),
-        ),
+        data=_scipp_variable_to_metadata_model(da.data),
         masks={key: _scipp_variable_to_model(mask) for key, mask in da.masks.items()},
         coords={
             key: _scipp_variable_to_model(coord) for key, coord in da.coords.items()
@@ -60,23 +70,31 @@ def extract_metadata(dg: sc.DataGroup | sc.DataArray) -> SciTiffMetadataContaine
     return SciTiffMetadataContainer(scitiffmeta=SciTiffMetadata(image=_metadata))
 
 
-def _export_data_array(da: sc.DataArray, file_path: str | pathlib.Path) -> None:
-    metadata = extract_metadata(da)
-    # Make sure the data is consistent with the metadata
+def _ensure_dimension_order(da: sc.DataArray) -> sc.DataArray:
+    default_sizes = {"x": 1, "y": 1, "z": 1, "t": 1, "c": 1}
+    final_sizes = {**default_sizes, **da.sizes}
+    # Order of the dimensions is according to the HyperStacks tiff format.
+    order = ["c", "t", "z", "y", "x"]
+    final_sizes = {key: final_sizes[key] for key in order if key in final_sizes}
+    dims = tuple(final_sizes.keys())
+    shape = tuple(final_sizes.values())
+    sizes: dict[str, int] = dict(zip(dims, shape, strict=True))
     # It is because ``z`` dimension and ``c`` dimension are often not present
     # but it is require by the HyperStacks and scitiffmeta schema.
     # Also, HyperStacks require specific order of dimensions.
-    dims = metadata.scitiffmeta.image.data.dims
-    shape = metadata.scitiffmeta.image.data.shape
-    sizes: dict[str, int] = dict(zip(dims, shape, strict=True))
-    final_image = sc.broadcast(da, sizes=sizes)
+    return sc.broadcast(da, sizes=sizes)
+
+
+def _export_data_array(da: sc.DataArray, file_path: str | pathlib.Path) -> None:
+    final_image = _ensure_dimension_order(da)
+    metadata = extract_metadata(final_image)
     tf.imwrite(
         file_path,
         final_image.values,
         imagej=True,
         metadata={
             key: json.dumps(value)
-            for key, value in metadata.model_dump(mode='json').items()
+            for key, value in metadata.model_dump(mode="json").items()
             # Tiff metadata will automatically be translated to json-like-string
             # so we need to convert the metadata to string in advance
             # to catch the error early and make sure it can be loaded back.
@@ -86,7 +104,7 @@ def _export_data_array(da: sc.DataArray, file_path: str | pathlib.Path) -> None:
 
 
 def _export_data_group(dg: sc.DataGroup, file_path: str | pathlib.Path) -> None:
-    raise NotImplementedError('Exporting DataGroup to SCITIFF is not yet implemented.')
+    raise NotImplementedError("Exporting DataGroup to SCITIFF is not yet implemented.")
 
 
 def export_scitiff(
@@ -98,19 +116,29 @@ def export_scitiff(
         _export_data_group(dg, file_path)
 
 
-def load_scitiff(file_path: str | pathlib.Path) -> sc.DataArray | sc.DataGroup:
+def load_scitiff(file_path: str | pathlib.Path, squeeze: bool = True) -> sc.DataGroup:
     with tf.TiffFile(file_path) as tif:
-        metadata = json.loads(tif.imagej_metadata['scitiffmeta'])
+        container = SciTiffMetadataContainer(
+            scitiffmeta=json.loads(tif.imagej_metadata["scitiffmeta"])
+        )
 
-    img = tf.imread(file_path, squeeze=True)
-    metadata = SciTiffMetadata(**metadata)
-    # # imread squeezes the dimensions, so we need to keep the original sizes
-    # # if not squeezed, the sizes have 1 extra dimension that I do not understand...
-    metadata_dict = metadata.model_dump(mode='json', exclude_none=True)
-    image_as_dict = metadata_dict['image']
-    sizes = dict(zip(metadata.image.data.dims, metadata.image.data.shape, strict=True))
-    sizes = {key: value for key, value in sizes.items() if value > 1}
-    image_as_dict['data']['values'] = img
-    image_as_dict['data']['dims'] = tuple(sizes.keys())
-    image_as_dict['data']['shape'] = tuple(sizes.values())
-    return from_dict(image_as_dict)  # TODO: Return datagroup including other metadata
+    metadata = container.scitiffmeta
+    metadata_dict = metadata.model_dump(mode="json")
+
+    img = tf.imread(file_path, squeeze=False)
+    if img.ndim == 6 and img.shape[-1] == 1:
+        # Grey scale image automatically wrap a pixel in a list
+        # so we need to remove the extra dimension
+        img = img.squeeze(axis=-1)
+
+    image_as_dict = metadata_dict["image"]
+    image_as_dict["data"]["values"] = img
+    image_da = from_dict(image_as_dict)
+    if squeeze:
+        # Squeeze the dimensions with size 1
+        to_be_squeezed_dims = tuple(
+            dim for dim, size in image_da.sizes.items() if size == 1
+        )
+        image_da = image_da.squeeze(dim=to_be_squeezed_dims)
+
+    return sc.DataGroup(image=image_da)
