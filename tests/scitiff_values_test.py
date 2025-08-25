f76534453b028c9471e6ef70fb78a2e64a87c926
YooSunyoung <17974113+YooSunYoung@users.noreply.github.com>
2025-08-25 08:39:43 +0200

    Add placeholder for values unit test.

---
+++ tests/scitiff_values_test.py
@@ -0,0 +1,3 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2025 Ess-dmsc-dram contributors (https://github.com/ess-dmsc-dram)
+# Placeholder for `values` api, that slices `Value` channels.
